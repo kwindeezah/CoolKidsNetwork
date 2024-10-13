d8401fe464d18e01d92792cc4421d309b9a37887
kwindeezah <hadizathmotunrayo@gmail.com>
2024-10-13 11:47:18 +0100

    Added a child theme

---
+++ wp-content/themes/twentytwentyfour-child/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function twenty_twenty_four_child_theme_enqueue_styles() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+}
+add_action('wp_enqueue_scripts', 'twenty_twenty_four_child_theme_enqueue_styles');
+
+?>
